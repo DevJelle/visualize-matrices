cb7f6638502754f9aeb41a4d8512c20ec248a2a8
DevJelle <jelle.123jvr@gmail.com>
2022-03-03 10:57:13 +0100

    reworked math + added GUI

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 import numpy as np
+from tkinter import *
 
-p = int(input("Matrix #rows:"))
-q = int(input("Matrix #columns:"))
-n = int(input("#Columns for A / #rows for B:"))
-
-print("Enter the elements for matrix A :")
-A = [[int(input()) for i in range(n)] for j in range(p)]
-print("Matrix A:")
-for i in range(p):
-    for j in range(n):
-        print(format(A[i][j], "<3"), end="")
-    print()
+root = Tk()
 
 
-print("Enter the elements for matrix B :")
-B = [[int(input()) for i in range(q)] for j in range(n)]
-print("Matrix B:")
-for i in range(n):
-    for j in range(q):
-        print(format(B[i][j], "<3"), end="")
-    print()
+myLabel = Label(root, text="Root test")
+myLabel.pack()
 
-result = [[0 for i in range(q)] for j in range(p)]
+root.mainloop()
 
-for i in range(p):
-    for j in range(q):
-        for k in range(n):
-            result[i][j] = result[i][j] + A[i][k] * A[k][j]
 
-print("Result:")
-for i in range(p):
-    for j in range(q):
-        print(format(result[i][j], "<3"), end="")
-    print()
+def consoleMultiplyOperator():
+    rows_a = int(input("Enter the Number of rows  for the first matrix: " ))
+    column_a = int(input("Enter the Number of Columns for the first matrix: "))
+
+    print("Enter the elements of First Matrix:")
+    matrix_a= [[int(input()) for i in range(column_a)] for i in range(rows_a)]
+
+    print("First Matrix is: ")
+    for n in matrix_a:
+        print(n)
+    #the number of columns of first matrix is equal to the number of rows of second matrix
+    column_b = int(input("Enter the Number of Columns for the second matrix: "))
+
+    print("Enter the elements of Second Matrix:")
+
+    matrix_b= [[int(input()) for i in range(column_b)] for i in range(column_a)]
+    for n in matrix_b:
+        print(n)
+        
+    result=[[0 for i in range(column_b)] for i in range(rows_a)]
+
+    for i in range(len(matrix_a)):
+        for j in range(len(matrix_b[0])):
+            for k in range(len(matrix_b)):
+                result [i][j]+=matrix_a[i][k]*matrix_b[k][j]
+    print("\nMatrix_a X Matrix_b is: ")
+    for r in result:
+        print(r)
+
+consoleMultiplyOperator()
